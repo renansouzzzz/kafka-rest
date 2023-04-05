ce11f82e2a2e33e0f681958cc9bc0a3825345a68
renansouzzzz <renansouza.oliv@outlook.com>
2023-04-05 10:27:12 -0300

    initial commit

---
+++ src/test/java/com/br/restkafka/RestKafkaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.br.restkafka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestKafkaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
